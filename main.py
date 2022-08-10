74dc7f7052888ac13b199b01df1d35cd8d3d5840
Rehan <rehans516@gmail.com>
2022-08-10 12:25:13 +0000

    check winning conditions

--- main.py
+++ main.py
@@ -4,10 +4,26 @@ def get_choices():
   player_choice = input("Enter a choice (rock, paper, scissors) : ")
   options = ['rock','paper','scissors']
   computer_choice = random.choice(options)
-  choices = {"player":player_choice,"computer":computer_choice}
+  choices = {"player": player_choice,"computer": computer_choice}
   return choices
 
-choices = get_choices()
-print(choices)
-
-
+def check_win(player, computer):
+  print(f"You chose {player} and computer chose {computer}")
+  if player == computer:
+    return "It's a tie!"
+  elif player == "rock":
+    if computer == "scissors":
+      return "Rock smashes scissors! You win!"
+    else:
+      return "Paper covers Rock! You lose."
+  elif player == "paper":
+    if computer == "rock":
+      return "Paper covers Rock! You win!."
+    else:
+      return "Scissors cuts paper! You lose."  
+  elif player == "scissors":
+    if computer == "rock":
+      return "Rock smashes scissors! You lose."
+    else:
+      return "Scissors cuts paper! You win!"
+      
